39c302e1e93f06e3b25f95884654c3dffac8ddae
Gabriel Perdue <perdue@fnal.gov>
2015-12-03 15:48:32 -0600

    grabbing data from the v10r8p8 area now

--- getter.sh
+++ getter.sh
@@ -4,7 +4,8 @@ filelist="skim_data_test_target0.dat
 skim_data_valid_target0.dat
 skim_data_learn_target0.dat"
 
-REMOTE_DIR="/minerva/app/users/perdue/cmtuser/Minerva_v10r8p7/Ana/NuclearTargetVertexing/ana/make_hist"
+MINERVA_RELEASE="v10r8p8"
+REMOTE_DIR="/minerva/app/users/perdue/cmtuser/Minerva_${MINERVA_RELEASE}/Ana/NuclearTargetVertexing/ana/make_hist"
 
 for file in $filelist
 do
