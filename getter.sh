2f2a700dc328e932a4b06691a66e9b35277c4069
Gabriel Perdue <perdue@fnal.gov>
2016-02-12 18:05:30 -0600

    Setup to grab the zsegments files

--- getter.sh
+++ getter.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-filelist="skim_data_test_target0.dat
-skim_data_valid_target0.dat
-skim_data_learn_target0.dat"
+fileroots="skim_data_test_zsegments
+skim_data_valid_zsegments
+skim_data_learn_zsegments"
 
 MINERVA_RELEASE="v10r8p8"
 REMOTE_DIR="/minerva/app/users/perdue/cmtuser/Minerva_${MINERVA_RELEASE}/Ana/NuclearTargetVertexing/ana/make_hist"
 
-for file in $filelist
+for file in $fileroots
 do
-  scp perdue@minervagpvm02.fnal.gov:${REMOTE_DIR}/$file .
+  for i in {2..3}
+  do
+    filenum=`echo $i | perl -ne 'printf "%04d",$_;'`
+    filename=${file}${filenum}.dat
+    scp perdue@minervagpvm02.fnal.gov:${REMOTE_DIR}/$filename .
+  done
 done
