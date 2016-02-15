41d7ca31e4d7a26280619146dd946bb859ebd195
Gabriel Perdue <perdue@fnal.gov>
2016-02-15 09:32:10 -0600

    make it easier to set up ranges

--- getter.sh
+++ getter.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+START=0
+STOP=7
+
+if [[ $# == 1 ]]; then
+  STOP=$1
+elif [[ $# > 1 ]]; then
+  START=$1
+  STOP=$2
+fi
+
+echo "Grabbing runs $START to $STOP..."
+
 fileroots="skim_data_test_zsegments
 skim_data_valid_zsegments
 skim_data_learn_zsegments"
@@ -9,10 +21,14 @@ REMOTE_DIR="/minerva/app/users/perdue/cmtuser/Minerva_${MINERVA_RELEASE}/Ana/Nuc
 
 for file in $fileroots
 do
-  for i in {2..3}
+  for i in `seq ${START} 1 ${STOP}`
   do
     filenum=`echo $i | perl -ne 'printf "%04d",$_;'`
     filename=${file}${filenum}.dat
+    echo $filename
     scp perdue@minervagpvm02.fnal.gov:${REMOTE_DIR}/$filename .
   done
 done
+
+# say we've finished
+echo -e "\a"
