115e77088b69658bcd407105331261dfe9ed348d
Gabriel Perdue <perdue@fnal.gov>
2016-02-27 17:52:46 -0600

    compactify and clean up with an internal function

--- Lasagne/minerva_ann_networks.py
+++ Lasagne/minerva_ann_networks.py
@@ -1,96 +1,59 @@
 #!/usr/bin/env python
 
 import lasagne
+from lasagne.layers import InputLayer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import ConcatLayer
 
 
-def build_triamese_alpha(input_var_x=None, input_var_u=None, input_var_v=None):
+def build_triamese_alpha(input_var_x=None, input_var_u=None, input_var_v=None,
+                         imgh=50, imgw=50):
     """
     'triamese' (one branch for each view, feeding a fully-connected network),
     model using two layers of convolutions and pooling.
     """
     # Input layer
-    l_in1_x = lasagne.layers.InputLayer(shape=(None, 1, 50, 50),
-                                        input_var=input_var_x)
-    l_in1_u = lasagne.layers.InputLayer(shape=(None, 1, 50, 50),
-                                        input_var=input_var_u)
-    l_in1_v = lasagne.layers.InputLayer(shape=(None, 1, 50, 50),
-                                        input_var=input_var_v)
+    tshape = (None, 1, imgh, imgw)
+    l_in1_x = InputLayer(shape=tshape, input_var=input_var_x)
+    l_in1_u = InputLayer(shape=tshape, input_var=input_var_u)
+    l_in1_v = InputLayer(shape=tshape, input_var=input_var_v)
 
-    # Convolutional layer with 32 kernels of size 3x3.
-    # Filtering reduces the image to (50-3+1, 50-3+1) = (48, 48), (ndim-filt+1)
-    # maxpooling reduces this further to (48/2, 48/2) = (24, 24), (dim/poolhw)
-    #
-    # NOTE: it isn't obvious I have h & w in the right "order" here...
-    filt_h1 = 3
-    filt_w1 = 3
-    filter_size1 = (filt_h1, filt_w1)
-    pool_size1 = (2, 2)
-    l_conv2d1_x = lasagne.layers.Conv2DLayer(
-        l_in1_x, num_filters=32, filter_size=filter_size1,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    l_conv2d1_u = lasagne.layers.Conv2DLayer(
-        l_in1_u, num_filters=32, filter_size=filter_size1,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    l_conv2d1_v = lasagne.layers.Conv2DLayer(
-        l_in1_v, num_filters=32, filter_size=filter_size1,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    # Max-pooling layer
-    l_maxpool1_x = lasagne.layers.MaxPool2DLayer(l_conv2d1_x,
-                                                 pool_size=pool_size1)
-    l_maxpool1_u = lasagne.layers.MaxPool2DLayer(l_conv2d1_u,
-                                                 pool_size=pool_size1)
-    l_maxpool1_v = lasagne.layers.MaxPool2DLayer(l_conv2d1_v,
-                                                 pool_size=pool_size1)
+    def make_branch(input_layer,
+                    num_filters1, filter_size1, pool_size1,
+                    num_filters2, filter_size2, pool_size2):
+        """
+        see: http://lasagne.readthedocs.org/en/latest/modules/layers.html
+        """
+        convlayer1 = Conv2DLayer(input_layer, num_filters=num_filters1,
+                                filter_size=filter_size1,
+                                nonlinearity=lasagne.nonlinearities.rectify,
+                                W=lasagne.init.GlorotUniform())
+        maxpoollayer1 = MaxPool2DLayer(convlayer1, pool_size=pool_size1)
+        convlayer2 = Conv2DLayer(maxpoollayer1, num_filters=num_filters2,
+                                filter_size=filter_size1,
+                                nonlinearity=lasagne.nonlinearities.rectify,
+                                W=lasagne.init.GlorotUniform())
+        maxpoollayer2 = MaxPool2DLayer(convlayer2, pool_size=pool_size2)
+        dense1 = DenseLayer(
+            lasagne.layers.dropout(maxpoollayer2, p=.5),
+            num_units=256,
+            nonlinearity=lasagne.nonlinearities.rectify)
+        return dense1
 
-    # More convolution and pooling layers...
-    # Filtering reduces the image to (24-3+1, 24-3+1) = (22, 22), (ndim-filt+1)
-    # maxpooling reduces this further to (22/2, 22/2) = (11, 11), (dim/poolhw)
-    filt_h2 = 3
-    filt_w2 = 3
-    filter_size2 = (filt_h2, filt_w2)
-    pool_size2 = (2, 2)
-    l_conv2d2_x = lasagne.layers.Conv2DLayer(
-        l_maxpool1_x, num_filters=32, filter_size=filter_size2,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    l_conv2d2_u = lasagne.layers.Conv2DLayer(
-        l_maxpool1_u, num_filters=32, filter_size=filter_size2,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    l_conv2d2_v = lasagne.layers.Conv2DLayer(
-        l_maxpool1_v, num_filters=32, filter_size=filter_size2,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    # Max-pooling layer
-    l_maxpool2_x = lasagne.layers.MaxPool2DLayer(l_conv2d2_x,
-                                                 pool_size=pool_size2)
-    l_maxpool2_u = lasagne.layers.MaxPool2DLayer(l_conv2d2_u,
-                                                 pool_size=pool_size2)
-    l_maxpool2_v = lasagne.layers.MaxPool2DLayer(l_conv2d2_v,
-                                                 pool_size=pool_size2)
-
-    # A fully-connected layer of 256 units with 50% dropout on its inputs:
-    l_dense1_x = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(l_maxpool2_x, p=.5),
-        num_units=256,
-        nonlinearity=lasagne.nonlinearities.rectify)
-    l_dense1_u = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(l_maxpool2_u, p=.5),
-        num_units=256,
-        nonlinearity=lasagne.nonlinearities.rectify)
-    l_dense1_v = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(l_maxpool2_v, p=.5),
-        num_units=256,
-        nonlinearity=lasagne.nonlinearities.rectify)
+    l_branch_x = make_branch(l_in1_x, 32, (3, 3), (2, 2),
+                             32, (3, 3), (2, 2))
+    l_branch_u = make_branch(l_in1_u, 32, (3, 3), (2, 2),
+                             32, (3, 3), (2, 2))
+    l_branch_v = make_branch(l_in1_v, 32, (3, 3), (2, 2),
+                             32, (3, 3), (2, 2))
 
     # Concatenate the two parallel inputs
-    l_concat = lasagne.layers.ConcatLayer((l_dense1_x, l_dense1_u, l_dense1_v))
+    l_concat = ConcatLayer((l_branch_x, l_branch_u, l_branch_v))
 
     # And, finally, the 11-unit output layer with 50% dropout on its inputs:
-    outp = lasagne.layers.DenseLayer(
+    outp = DenseLayer(
         lasagne.layers.dropout(l_concat, p=.5),
         num_units=11,
         nonlinearity=lasagne.nonlinearities.softmax)
