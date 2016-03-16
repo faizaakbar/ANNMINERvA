f21d03442bd1bca7424ee69e6cfdec48e9bad2ff
Gabriel Perdue <perdue@fnal.gov>
2016-03-16 18:38:16 -0500

    Program to make a string of all the views in the order they appear

---
+++ code_archive/plane_views_snippet.cpp
@@ -0,0 +1,865 @@
+#include <iostream>
+int main()
+{
+  for (unsigned int module = 0; module < 120; ++module) {
+    for (unsigned int plane = 1; plane < 3; ++plane) {
+      if (module == 0) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 1) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 2) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 3) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 5) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 6) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 7) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 8) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 10) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 11) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 12) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 13) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 16) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 17) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 18) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 19) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 20) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 21) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 22) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 23) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 25) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 26) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 28) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 29) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 30) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 31) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 32) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 33) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 34) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 35) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 36) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 37) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 38) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 39) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 40) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 41) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 42) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 43) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 44) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 45) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 46) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 47) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 48) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 49) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 50) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 51) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 52) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 53) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 54) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 55) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 56) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 57) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 58) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 59) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 60) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 61) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 62) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 63) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 64) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 65) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 66) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 67) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 68) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 69) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 70) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 71) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 72) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 73) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 74) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 75) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 76) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 77) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 78) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 79) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 80) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 81) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 82) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 83) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 84) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 85) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 86) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 87) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 88) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 89) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 90) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 91) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 92) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 93) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 94) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 95) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 96) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 97) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 98) {
+        if (plane == 1) {
+          std::cout << "U";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 99) {
+        if (plane == 1) {
+          std::cout << "V";
+        }
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 100) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 101) {
+        if (plane == 2) {
+          std::cout << "U";
+        }
+      }
+      if (module == 102) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 103) {
+        if (plane == 2) {
+          std::cout << "V";
+        }
+      }
+      if (module == 104) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 105) {
+        if (plane == 2) {
+          std::cout << "U";
+        }
+      }
+      if (module == 106) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 107) {
+        if (plane == 2) {
+          std::cout << "V";
+        }
+      }
+      if (module == 108) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 109) {
+        if (plane == 2) {
+          std::cout << "U";
+        }
+      }
+      if (module == 110) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 111) {
+        if (plane == 2) {
+          std::cout << "V";
+        }
+      }
+      if (module == 112) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 113) {
+        if (plane == 2) {
+          std::cout << "U";
+        }
+      }
+      if (module == 114) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 115) {
+        if (plane == 2) {
+          std::cout << "V";
+        }
+      }
+      if (module == 116) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 117) {
+        if (plane == 2) {
+          std::cout << "U";
+        }
+      }
+      if (module == 118) {
+        if (plane == 2) {
+          std::cout << "X";
+        }
+      }
+      if (module == 119) {
+        if (plane == 2) {
+          std::cout << "V";
+        }
+      }
+    }
+  }
+
+  std::cout << std::endl;
+  return 0;
+}
+
+
