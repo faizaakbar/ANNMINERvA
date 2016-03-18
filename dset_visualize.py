1a0bd7a5d471e3beacc3f8256de576de791d9b8d
Gabriel Perdue <perdue@fnal.gov>
2016-03-18 15:25:09 -0500

    Rename output files to include actual run, subrun, etc.

--- dset_visualize.py
+++ dset_visualize.py
@@ -29,24 +29,45 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
     max_evts = int(sys.argv[2])
 
+
+def decode_eventid(eventid):
+    """
+    assume encoding from fuel_up_nukecc.py, etc.
+    """
+    eventid = str(eventid)
+    phys_evt = eventid[-2:]
+    eventid = eventid[:-2]
+    gate = eventid[-4:]
+    eventid = eventid[:-4]
+    subrun = eventid[-4:]
+    eventid = eventid[:-4]
+    run = eventid
+    return (run, subrun, gate, phys_evt)
+
 f = h5py.File(filename, 'r')
 valid_data = pylab.zeros(pylab.shape(f['hits']), dtype='f')
 valid_labels = pylab.zeros(pylab.shape(f['segments']), dtype='f')
+valid_evtids = pylab.zeros(pylab.shape(f['eventids']), dtype='uint64')
 f['hits'].read_direct(valid_data)
 f['segments'].read_direct(valid_labels)
+f['eventids'].read_direct(valid_evtids)
 f.close()
 
 for counter, evt in enumerate(valid_data):
     if evt_plotted > max_evts:
         break
     targ = valid_labels[counter]
+    evtid = valid_evtids[counter]
+    run, subrun, gate, phys_evt = decode_eventid(evtid)
+    print('{} - {} - {} - {}'.format(run, subrun, gate, phys_evt))
     fig = pylab.figure(figsize=(9, 3))
     gs = pylab.GridSpec(1, 3)
     for i in range(3):
         ax = pylab.subplot(gs[i])
         ax.axis('off')
         ax.imshow(evt[i])
-    figname = 'evt_%d_targ_%d.pdf' % (counter, targ)
+    figname = 'evt_%s_%s_%s_%s_targ_%d.pdf' % \
+        (run, subrun, gate, phys_evt, targ)
     pylab.savefig(figname)
     pylab.close()
     evt_plotted += 1
