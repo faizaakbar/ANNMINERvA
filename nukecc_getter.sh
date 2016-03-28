f90609d30a02c8bcfbb51205c9fb1551aff7f095
Gabriel Perdue <perdue@fnal.gov>
2016-03-28 17:54:06 -0500

    new file paths

--- nukecc_getter.sh
+++ nukecc_getter.sh
@@ -12,12 +12,13 @@ fi
 
 echo "Grabbing runs $START to $STOP..."
 
-fileroots="minosmatch_127x94_me1Bmc_"
 fileroots="minosmatch_127x94_me1Bmc_micro_"
+fileroots="minosmatch_127x94_me1Bmc_"
+fileroots="minosmatch_nukecczdefs_127x94_me1Bmc_"
 
 MINERVA_RELEASE="v10r8p8"
 REMOTE_DIR="/minerva/app/users/perdue/cmtuser/Minerva_${MINERVA_RELEASE}/Ana/NuclearTargetVertexing/ana/make_hist"
-REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/nukeccskimmer_minosmatch_127x94"
+REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/nukeccskimmer_minosmatch_127x94_nukecczdefs"
 
 for file in $fileroots
 do
