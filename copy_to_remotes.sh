99d057a96fc6b2987ccf6533a10b77d87a2ea3d6
Gabriel Perdue <perdue@fnal.gov>
2016-04-14 16:47:50 -0500

    I am lazy

---
+++ copy_to_remotes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+filelist="
+minosmatch_nukecczdefs_127x25_u_me1Bmc.hdf5
+minosmatch_nukecczdefs_127x25_v_me1Bmc.hdf5
+minosmatch_nukecczdefs_127x50x25_xuv_me1Bmc.hdf5
+minosmatch_nukecczdefs_127x25_u_me1Bmc_README.txt
+minosmatch_nukecczdefs_127x25_v_me1Bmc_README.txt
+minosmatch_nukecczdefs_127x50x25_xuv_me1Bmc_README.txt"
+
+REMOTE1="tev.fnal.gov:/phihome/perdue/theano/data/"
+REMOTE2="minervagpvm02.fnal.gov:/minerva/data/users/perdue/mlmpr/nukecc"
+for file in $filelist
+do
+  scp $file perdue@${REMOTE1}
+  scp $file perdue@${REMOTE2}
+done
+
+mkdir temp_for_copy
+for file in $filelist
+do
+  cp $file temp_for_copy
+done
+tar -cvzf temp_for_copy.tgz temp_for_copy
+rm -rf temp_for_copy
+echo "Next, run:"
+echo " scp temp_for_copy.tgz gnperdue@titan.ccs.ornl.gov:/ccs/proj/hep105/data/theano"
