29ea6b9e7ae08fab51db40f5d665a360ce14dc02
Gabriel Perdue <perdue@fnal.gov>
2016-04-29 10:44:17 -0500

    modify the inputs to the pre-training/transfer-learning study

--- copy_to_remotes.sh
+++ copy_to_remotes.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 filelist="
-minosmatch_nukecczdefs_127x50x25_xuv_me1Amc.hdf5
-minosmatch_nukecczdefs_127x50x25_xuv_me1Amc_README.txt"
+minosmatch_nukecczdefs_127x50_x_me1Bmc.hdf5
+minosmatch_nukecczdefs_127x68_x_padded_me1Bmc_README.txt
+minosmatch_nukecczdefs_127x50_x_me1Bmc_README.txt"
 
 REMOTE1="tev.fnal.gov:/phihome/perdue/theano/data/"
 REMOTE2="minervagpvm02.fnal.gov:/minerva/data/users/perdue/mlmpr/nukecc"
@@ -12,12 +13,12 @@ do
   scp $file perdue@${REMOTE2}
 done
 
-# mkdir temp_for_copy
-# for file in $filelist
-# do
-#   cp $file temp_for_copy
-# done
-# tar -cvzf temp_for_copy.tgz temp_for_copy
-# rm -rf temp_for_copy
-# echo "Next, run:"
-# echo " scp temp_for_copy.tgz gnperdue@titan.ccs.ornl.gov:/ccs/proj/hep105/data/theano"
+mkdir temp_for_copy
+for file in $filelist
+do
+  cp $file temp_for_copy
+done
+tar -cvzf temp_for_copy.tgz temp_for_copy
+rm -rf temp_for_copy
+echo "Next, run:"
+echo " scp temp_for_copy.tgz gnperdue@titan.ccs.ornl.gov:/ccs/proj/hep105/data/theano"
