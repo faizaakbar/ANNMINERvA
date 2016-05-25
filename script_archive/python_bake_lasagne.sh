66d7c9229aaa7487b4a943991d84437122470836
Gabriel Perdue <perdue@fnal.gov>
2016-05-25 16:41:10 -0500

    updated for new paths on WC

--- script_archive/python_bake_lasagne.sh
+++ script_archive/python_bake_lasagne.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=`cat /tmp/pbs.prologue.$PBS_JOBID`
+
 export PATH=/usr/local/python2/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/python2/lib:$LD_LIBRARY_PATH
 
@@ -7,4 +9,5 @@ export LD_LIBRARY_PATH=/usr/local/python2/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/hdf5/lib:$LD_LIBRARY_PATH
 
 # nvcc compiler - maybe wrap in a hostname check for gpus...
-export PATH=/usr/local/cuda/bin:$PATH
+export PATH=/usr/local/cuda-7.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
