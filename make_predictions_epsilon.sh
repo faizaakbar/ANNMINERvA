66194da2a2af58f5fabd0d31c1118140b5ea40f1
Gabriel Perdue <perdue@fnal.gov>
2016-07-03 13:54:50 -0500

    don't test also

--- make_predictions_epsilon.sh
+++ make_predictions_epsilon.sh
@@ -6,7 +6,7 @@ MODEL=lminervatriamese_epsilon${DATE}.npz
 python Lasagne/minerva_triamese_epsilon.py \
   -d minosmatch_nukecczdefs_127x50x25_xuv_me1Amc.hdf5 \
   -p \
-  -t \
   -a \
   -s models/${MODEL} | tee evt_log_me1A_epsilon${DATE}.txt
+  # -t \
   # -v \
