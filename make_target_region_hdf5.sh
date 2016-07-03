8000f8cd4d8bf583d1289707998b4689f4b0155a
Gabriel Perdue <perdue@fnal.gov>
2016-07-03 13:54:19 -0500

    process minerva1 files

--- make_target_region_hdf5.sh
+++ make_target_region_hdf5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python make_hdf5_fuelfiles.py \
-  -b minosmatch_nukecczdefs_fullz_127x94_me1Bmc \
-  -o minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_me1Bmc.hdf5 \
+  -b minosmatch_nukecczdefs_fullz_127x94_minerva1mc \
+  -o minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_minerva1mc.hdf5 \
   -x \
   --trim_column_down_x 50 \
   --trim_column_down_uv 25 \
