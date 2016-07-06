08b5f74a67468174270af8a5882d5fae286e20fc
Gabriel Perdue <perdue@fnal.gov>
2016-07-06 13:04:29 -0500

    increase flexibility a bit

--- make_predictions_epsilon.sh
+++ make_predictions_epsilon.sh
@@ -3,10 +3,16 @@
 DATE="1465065844"
 MODEL=lminervatriamese_epsilon${DATE}.npz
 
+# DATAFILE=minosmatch_nukecczdefs_127x50x25_xuv_me1Amc.hdf5
+DATAFILE=minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_minerva1mc_0000.hdf5
+
+# LOGFILE=evt_log_me1A_epsilon${DATE}.txt
+LOGFILE=evt_log_minerva1_epsilon${DATE}_0000.txt
+
 python Lasagne/minerva_triamese_epsilon.py \
-  -d minosmatch_nukecczdefs_127x50x25_xuv_me1Amc.hdf5 \
+  -d $DATAFILE \
   -p \
   -a \
-  -s models/${MODEL} | tee evt_log_me1A_epsilon${DATE}.txt
+  -s models/${MODEL} | tee $LOGFILE
   # -t \
   # -v \
