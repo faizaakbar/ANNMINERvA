e5cca6ec41ae49336de2a77d5638ccd2b993ff15
Gabriel Perdue <perdue@fnal.gov>
2016-07-06 12:37:54 -0500

    improve re-usability a bit

--- make_hadron_singletons_hdf5.sh
+++ make_hadron_singletons_hdf5.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# process hadron dat.gz (text) files into hdf5 files for the general hadron
+# multiplicity and pi0 outputs
+
 START=0
 STOP=0
 
@@ -10,16 +13,34 @@ elif [[ $# > 1 ]]; then
   STOP=$2
 fi
 
+PLAYLIST=minerva1
+MC=mc
+INPUT_BASENAME=minosmatch_hadmult_${PLAYLIST}${MC}
+HADMULT_OUTPUTNAME=minosmatch_hadmult_${PLAYLIST}${MC}
+SINGLEPI0_OUTPUTNAME=minosmatch_singlepi0_${PLAYLIST}${MC}
+
 for i in `seq ${START} 1 ${STOP}`
 do
   paddednum=`echo $i | perl -ne 'printf "%04d",$_;'`
+cat << EOF
   python make_hdf5_fuelfiles.py \
-    -b minosmatch_hadmult_me1Amc_${paddednum} \
-    -o minosmatch_hadmult_me1Amc_${paddednum}.hdf5 \
+    -b ${INPUT_BASENAME}_${paddednum} \
+    -o ${HADMULT_OUTPUTNAME}_${paddednum}.hdf5 \
     --skim had_mult
+EOF
   python make_hdf5_fuelfiles.py \
-    -b minosmatch_hadmult_me1Amc_${paddednum} \
-    -o minosmatch_singlepi0_me1Amc_${paddednum}.hdf5 \
+    -b ${INPUT_BASENAME}_${paddednum} \
+    -o ${HADMULT_OUTPUTNAME}_${paddednum}.hdf5 \
+    --skim had_mult
+cat << EOF
+  python make_hdf5_fuelfiles.py \
+    -b ${INPUT_BASENAME}_${paddednum} \
+    -o ${SINGLEPI0_OUTPUTNAME}_${paddednum}.hdf5 \
+    --skim single_pi0
+EOF
+  python make_hdf5_fuelfiles.py \
+    -b ${INPUT_BASENAME}_${paddednum} \
+    -o ${SINGLEPI0_OUTPUTNAME}_${paddednum}.hdf5 \
     --skim single_pi0
 done
 
