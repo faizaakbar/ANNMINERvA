6b657cad5904b6b0780589de93d08cbe5a5ca905
Gabriel Perdue <perdue@fnal.gov>
2016-07-08 18:07:09 -0500

    get different files

--- nukecc_getter.sh
+++ nukecc_getter.sh
@@ -14,11 +14,11 @@ echo "Grabbing runs $START to $STOP..."
 
 fileroots="minosmatch_nukecczdefs_fullz_127x94_minerva1mc_"
 fileroots="minosmatch_hadmult_minerva1mc_"
-fileroots="minosmatch_muondat_me1Amc_"
+fileroots="minosmatch_muondat_me1Bmc_"
 
 REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/nukeccskimmer_minosmatch_127x94_nukecczdefs"
 REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/hadmult_skims/cvs_rev1_1"
-REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/muon_skims/cvs_rev1_2"
+REMOTE_DIR="/minerva/data/users/perdue/mlmpr/raw_dat/muon_skims/cvs_rev1_3"
 
 for file in $fileroots
 do
