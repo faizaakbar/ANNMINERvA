ea378f5bf208e9d66a5226a7bb650267fd2a4430
Gabriel Perdue <perdue@fnal.gov>
2016-07-12 17:59:21 -0500

    make muon singletons

---
+++ make_muon_singletons.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# process image dat.gz (text) files into hdf5 files for the tracker and
+# target regions.
+
+START=0
+STOP=0
+
+if [[ $# == 1 ]]; then
+  STOP=$1
+elif [[ $# > 1 ]]; then
+  START=$1
+  STOP=$2
+fi
+
+INPUT_BASENAME=minosmatch_muondat_me1Bmc
+OUTPUTNAME=minosmatch_muondat_me1Bmc
+
+for i in `seq ${START} 1 ${STOP}`
+do
+  paddednum=`echo $i | perl -ne 'printf "%04d",$_;'`
+  python make_hdf5_fuelfiles.py \
+    -b ${INPUT_BASENAME}_${paddednum} \
+    -o ${OUTPUTNAME}_${paddednum}.hdf5 \
+    -s muon_dat
+done
