08acd4288df822fac65eea7af2999b1bf67d8f12
Gabriel Perdue <perdue@fnal.gov>
2016-07-12 17:58:30 -0500

    temp work

--- make_fulldet_hdf5.sh
+++ make_fulldet_hdf5.sh
@@ -10,12 +10,15 @@ elif [[ $# > 1 ]]; then
   STOP=$2
 fi
 
+INBASE=ztest_minerva1mc
+OUTBASE=ztest_minerva1mc
+
 for i in `seq ${START} 1 ${STOP}`
 do
   paddednum=`echo $i | perl -ne 'printf "%04d",$_;'`
   python make_hdf5_fuelfiles.py \
-    -b minosmatch_nukecczdefs_fullz_127x94_me1Amc_${paddednum} \
-    -o minosmatch_nukecczdefs_tracker_127x72x36_me1Amc_${paddednum}.hdf5 \
+    -b ${INBASE}_${paddednum} \
+    -o ${OUTBASE}_${paddednum}.hdf5 \
     -x \
     --trim_column_up_x 22 --trim_column_down_x 94 \
     --trim_column_up_uv 11 --trim_column_down_uv 47 \
