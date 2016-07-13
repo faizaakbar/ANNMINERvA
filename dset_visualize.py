a875e793779391637395569339d9e482bc918342
Gabriel Perdue <perdue@fnal.gov>
2016-07-13 17:24:56 -0500

    add the ability to plot pixel-mapped relative timing data

--- dset_visualize.py
+++ dset_visualize.py
@@ -52,61 +52,100 @@ def decode_eventid(eventid):
 
 f = h5py.File(filename, 'r')
 
+have_times = False
 # look for x, u, v data hits
 try:
     data_x_shp = pylab.shape(f['hits-x'])
+    xname = 'hits-x'
 except KeyError:
     print("'hits-x' does not exist.")
     data_x_shp = None
 try:
     data_u_shp = pylab.shape(f['hits-u'])
+    uname = 'hits-u'
 except KeyError:
     print("'hits-u' does not exist.")
     data_u_shp = None
 try:
     data_v_shp = pylab.shape(f['hits-v'])
+    vname = 'hits-v'
 except KeyError:
     print("'hits-v' does not exist.")
     data_v_shp = None
+# maybe we have times instead...
+if data_x_shp is None:
+    try:
+        data_x_shp = pylab.shape(f['times-x'])
+        have_times = True
+        xname = 'times-x'
+    except KeyError:
+        print("'times-x' does not exist.")
+        data_x_shp = None
+if data_u_shp is None:
+    try:
+        data_u_shp = pylab.shape(f['times-u'])
+        have_times = True
+        uname = 'times-u'
+    except KeyError:
+        print("'times-u' does not exist.")
+        data_u_shp = None
+if data_v_shp is None:
+    try:
+        data_v_shp = pylab.shape(f['times-v'])
+        have_times = True
+        vname = 'times-v'
+    except KeyError:
+        print("'times-v' does not exist.")
+        data_v_shp = None
 
 # if we have hits, get them, else set those containers to None
 if data_x_shp is not None:
     data_x_shp = (max_evts, data_x_shp[1], data_x_shp[2], data_x_shp[3])
     data_x = pylab.zeros(data_x_shp, dtype='f')
-    data_x = f['hits-x'][:max_evts]
+    data_x = f[xname][:max_evts]
 else:
     data_x = None
 if data_u_shp is not None:
     data_u_shp = (max_evts, data_u_shp[1], data_u_shp[2], data_u_shp[3])
     data_u = pylab.zeros(data_u_shp, dtype='f')
-    data_u = f['hits-u'][:max_evts]
+    data_u = f[uname][:max_evts]
 else:
     data_u = None
 if data_v_shp is not None:
     data_v_shp = (max_evts, data_v_shp[1], data_v_shp[2], data_v_shp[3])
     data_v = pylab.zeros(data_v_shp, dtype='f')
-    data_v = f['hits-v'][:max_evts]
+    data_v = f[vname][:max_evts]
 else:
     data_v = None
+
 labels_shp = (max_evts,)
 evtids_shp = (max_evts,)
 labels = pylab.zeros(labels_shp, dtype='f')
 evtids = pylab.zeros(evtids_shp, dtype='uint64')
-labels = f['segments'][:max_evts]
-pcodes = f['planecodes'][:max_evts]
 evtids = f['eventids'][:max_evts]
-zs = f['zs'][:max_evts]
+try:
+    labels = f['segments'][:max_evts]
+    pcodes = f['planecodes'][:max_evts]
+    zs = f['zs'][:max_evts]
+except KeyError:
+    labels_shp = None
+
 f.close()
 
 for counter, evtid in enumerate(evtids):
     if evt_plotted > max_evts:
         break
     run, subrun, gate, phys_evt = decode_eventid(evtid)
-    targ = labels[counter]
-    pcode = pcodes[counter]
-    zz = zs[counter]
-    print('{} - {} - {} - {}: tgt: {:02d}; plncd {:03d}; z {}'.format(
-        run, subrun, gate, phys_evt, targ, pcode, zz))
+    if labels_shp is not None:
+        targ = labels[counter]
+        pcode = pcodes[counter]
+        zz = zs[counter]
+        pstring = '{} - {} - {} - {}: tgt: {:02d}; plncd {:03d}; z {}'.format(
+            run, subrun, gate, phys_evt, targ, pcode, zz)
+    else:
+        pstring = '{} - {} - {} - {}'.format(
+            run, subrun, gate, phys_evt)
+    print(pstring)
     evt = []
     if data_x is not None:
         evt.append(data_x[counter])
@@ -124,10 +163,22 @@ for counter, evtid in enumerate(evtids):
         # images are normalized such the max e-dep has val 1, independent
         # of view, so set vmin, vmax here to keep matplotlib from
         # normalizing each view on its own
-        ax.imshow(evt[i][0], cmap=pylab.get_cmap('jet'),
-                  interpolation='nearest', vmin=0, vmax=1)
-    figname = 'evt_%s_%s_%s_%s_targ_%d_pcode_%d.pdf' % \
-        (run, subrun, gate, phys_evt, targ, pcode)
+        minv = 0
+        cmap = 'jet'
+        if have_times:
+            minv = -1
+            cmap = 'seismic'
+            ax.axis('on')
+            ax.xaxis.set_major_locator(pylab.NullLocator())
+            ax.yaxis.set_major_locator(pylab.NullLocator())
+        ax.imshow(evt[i][0], cmap=pylab.get_cmap(cmap),
+                  interpolation='nearest', vmin=minv, vmax=1)
+    if labels_shp is not None:
+        figname = 'evt_%s_%s_%s_%s_targ_%d_pcode_%d.pdf' % \
+                  (run, subrun, gate, phys_evt, targ, pcode)
+    else:
+        figname = 'evt_%s_%s_%s_%s.pdf' % \
+                  (run, subrun, gate, phys_evt)
     pylab.savefig(figname)
     pylab.close()
     evt_plotted += 1
