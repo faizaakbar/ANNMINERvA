bbb915860b65e973c80b68bd85a4b299e6961ca0
Gabriel Perdue <perdue@fnal.gov>
2016-09-13 14:04:11 -0500

    fix load lib path problem for boost's silliness

--- WilsonCluster/setup_scripts/caffe_setup.sh
+++ WilsonCluster/setup_scripts/caffe_setup.sh
@@ -5,6 +5,7 @@ export PATH=/usr/local/protobuf/bin:$PATH
 export PATH=/usr/local/caffe.gradient-reversal/bin:$PATH #or use caffe.latest
 export PATH=/usr/local/cuda/bin:$PATH
 
+export LD_LIBRARY_PATH=/usr/local/boost_1_59_0/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/gcc-4.9.1/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/gcc-4.9.1/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
