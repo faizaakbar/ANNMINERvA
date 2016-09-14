dd74370612d86e77434cb11cafb0526af7d93429
Gabriel Perdue <perdue@fnal.gov>
2016-09-14 14:05:06 -0500

    start to keep Caffe stuff here too

---
+++ Caffe/vertex/install_proto_solvers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+BASEDIR=/phihome/perdue/caffe
+cp vertex_epsilon_adv.solver $BASEDIR/solvers
+cp vertex_epsilon_adv.prototxt $BASEDIR/proto
---
+++ Caffe/vertex/list_maker.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# vertex adv trainlist based on ME 1B MC (source domain)
+TRAINMIN=0
+TRAINMAX=75
+VALIDMIN=76
+VALIDMAX=81
+TESTMIN=82
+TESTMAX=88
+BASED=/phihome/perdue/caffe/data/minerva/vertex
+FILEROOT=minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_me1Bmc
+OUTROOT=vertex_epsilon_adv_source
+rm -f $OUTROOT.trainlist
+rm -f $OUTROOT.validlist
+rm -f $OUTROOT.testlist
+for i in `seq $TRAINMIN 1 $TRAINMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.trainlist
+done
+for i in `seq $VALIDMIN 1 $VALIDMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.validlist
+done
+for i in `seq $TESTMIN 1 $TESTMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/unrenamed/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.testlist
+done
+
+
+# vertex adv trainlist based on LE Minerva1 & Minerva 13B MC (target domain)
+# (no validation here - these are DANN partners...)
+TRAINMIN=0
+TRAINMAX=40
+TESTMIN=41
+TESTMAX=44
+BASED=/phihome/perdue/caffe/data/minerva/vertex
+FILEROOT=minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_minerva1mc
+OUTROOT=vertex_epsilon_adv_target
+rm -f $OUTROOT.trainlist
+rm -f $OUTROOT.testlist
+for i in `seq $TRAINMIN 1 $TRAINMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.trainlist
+done
+for i in `seq $TESTMIN 1 $TESTMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.testlist
+done
+# vertex adv trainlist based on LE Minerva1 & Minerva 13B MC (target domain), cont
+TRAINMIN=0
+TRAINMAX=3
+TESTMIN=4
+TESTMAX=7
+FILEROOT=minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_minerva13Bmc
+for i in `seq $TRAINMIN 1 $TRAINMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.trainlist
+done
+for i in `seq $TESTMIN 1 $TESTMAX`
+do
+  filenum=`echo $i | perl -ne 'printf "%03d",$_;'`
+  ls -1 ${BASED}/${FILEROOT}_${filenum}.hdf5 >> $OUTROOT.testlist
+done
