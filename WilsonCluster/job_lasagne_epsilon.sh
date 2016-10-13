2c7a10d4be4be6de8c79feac38650d10d960c219
Gabriel Perdue <perdue@fnal.gov>
2016-10-13 13:33:42 -0500

    comment the action back in

--- WilsonCluster/job_lasagne_epsilon.sh
+++ WilsonCluster/job_lasagne_epsilon.sh
@@ -67,14 +67,13 @@ python ${PYTHONPROG} -l $DOTEST \
   -f $LOGFILENAME
 EOF
 export THEANO_FLAGS=device=gpu,floatX=float32
-python test.py
-# python ${PYTHONPROG} -l $DOTEST \
-#   -n $NEPOCHS \
-#   -r $LRATE \
-#   -g $L2REG \
-#   -s $SAVEMODELNAME \
-#   -d $DATAFILENAME \
-#   -f $LOGFILENAME
+python ${PYTHONPROG} -l $DOTEST \
+  -n $NEPOCHS \
+  -r $LRATE \
+  -g $L2REG \
+  -s $SAVEMODELNAME \
+  -d $DATAFILENAME \
+  -f $LOGFILENAME
 
 echo "Job ${PBS_JOBNAME} submitted from ${PBS_O_HOST} finished "`date`" jobid ${PBS_JOBID}"
 exit 0
