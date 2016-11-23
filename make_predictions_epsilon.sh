381420f95054c4b1b0a353ebf2e5dbed69e79f23
Gabriel Perdue <perdue@fnal.gov>
2016-11-23 15:21:30 -0600

    some modernization

--- make_predictions_epsilon.sh
+++ make_predictions_epsilon.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-DATE="1465065844"
-MODEL=lminervatriamese_epsilon${DATE}.npz
+MODEL="models/lminerva_spacetime_11_epsilon1479750354.npz"
+DATAFILE="../HDF5files/minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_000.hdf5"
+LOGFILE=spacetime_epsilon_me1Bmc_1479750354_test.txt
+PERFMAT=perfmat11_me1Bmc_epsilon1479750354.npy
 
-# DATAFILE=minosmatch_nukecczdefs_127x50x25_xuv_me1Amc.hdf5
-DATAFILE=minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_minerva1mc_0000.hdf5
-
-# LOGFILE=evt_log_me1A_epsilon${DATE}.txt
-LOGFILE=evt_log_minerva1_epsilon${DATE}_0000.txt
+TGTIDX=5
+NOUTPUTS=11
+# TGTIDX=4
+# NOUTPUTS=67
 
 # `-p` == make predictions (db file)
 # `-t` == run test (no db file)
 # `-v` == run in verbose mode
 # `-a` == use the whole file as if it were the "test" sample
-python Lasagne/minerva_triamese_epsilon.py \
+python Lasagne/minerva_tricolumnar_spacetime_epsilon.py \
   -d $DATAFILE \
   -t \
   -v \
   -a \
-  -s models/${MODEL} | tee $LOGFILE
+  -s $MODEL \
+  -f $LOGFILE \
+  --target_idx $TGTIDX \
+  --noutputs $NOUTPUTS
   # -p \
+
+mv perfmat${NOUTPUTS}.npy $PERFMAT
