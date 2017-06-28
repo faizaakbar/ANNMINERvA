565ce36a60218f8293a955b201efb5ac1619862d
Gabriel Perdue <gnperdue@me.com>
2017-06-28 17:13:39 -0500

    fewer lines of output, but include dtype

--- examine_hdf5.py
+++ examine_hdf5.py
@@ -14,26 +14,20 @@ if '-h' in sys.argv or '--help' in sys.argv:
     print(__doc__)
     sys.exit(1)
 
-
-def printname(name):
-    print(name)
-
 filename = sys.argv[1]
 f = h5py.File(filename, 'r')
 
 print("\ngroups in the hdf5 file")
 print("-----------------------")
-print(f.visit(printname))
-
-print("\nsizes of groups in the hdf5 file")
-print("--------------------------------")
-for dset in f:
-    print(np.shape(f[dset]))
-
+for name in f:
+    print('{:>12}: {:>8}: shape = {}'.format(
+        name, np.dtype(f[name]), np.shape(f[name])
+    ))
+    
 print("\nto examine data, type things like:")
 print("----------------------------------")
 print("f['mygroup'][:5]     - 1st 5 items")
 print("f['mygroup'][0][:5]  - 1st 5 items in second index of first item")
 print(" - etc. - basically you may use numpy slice notation")
 
-f.close
+f.close()
