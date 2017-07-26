1675fe59e9864213c19f0056c1c82ccad5773ea9
Gabriel Perdue <gnperdue@me.com>
2017-07-26 12:54:20 -0500

    simple text recorder

--- TensorFlow/MnvRecorderText.py
+++ TensorFlow/MnvRecorderText.py
@@ -2,6 +2,7 @@
 """
 Do persistence
 """
+from __future__ import print_function
 import logging
 
 import mnv_utils
@@ -13,9 +14,20 @@ class MnvCategoricalTextRecorder:
     """
     record segments or planecodes in a sqlite db
     """
-    def __init__(self):
-        pass
+    def __init__(self, db_base_name):
+        self.db_name = db_base_name + '.txt'
 
-    def write_data(eventid, pred, probs):
+    def write_data(self, eventid, pred, probs):
+        with open(self.db_name, 'a+') as f:
+            evtid_string = ','.join(mnv_utils.decode_eventid(eventid))
+            probs_string = ','.join([str(i) for i in probs])
+            msg = evtid_string + ',' + str(pred) + ',' + probs_string + '\n'
+            f.write(msg)
         return None
 
+    def read_data(self):
+        """ do not call this on large files """
+        with open(self.db_name, 'r') as f:
+            content = f.readlines()
+            content = [x.strip() for x in content]
+        return content
