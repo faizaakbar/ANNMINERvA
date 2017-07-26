f8583548fca6473e1a47bb006996c205145c5951
Gabriel Perdue <gnperdue@me.com>
2017-07-26 09:19:35 -0500

    add placeholder class for recording predictions to a text file

---
+++ TensorFlow/MnvRecorderText.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Do persistence
+"""
+import logging
+
+import mnv_utils
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MnvCategoricalTextRecorder:
+    """
+    record segments or planecodes in a sqlite db
+    """
+    def __init__(self):
+        pass
+
+    def write_data(eventid, pred, probs):
+        return None
+
