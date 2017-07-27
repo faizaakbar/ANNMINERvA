62578c083a156f316b1dfa956700b101e4768a4e
Gabriel Perdue <perdue@fnal.gov>
2017-07-27 09:17:40 -0500

    start to work on submission script for file converion that uses singularity

---
+++ TensorFlow/singularity_process_hdf5_to_tfrecord.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# file creation parameters
+NEVTS=1000
+MAXTRIPS=3
+TRAINFRAC=0.88
+VALIDFRAC=0.06
+
+# tag the log file
+DAT=`date +%s`
+
+# which singularity image
+SNGLRTY="/data/perdue/singularity/simone/ubuntu16-cuda-ml.img"
+
+# file logistics
+HDF5DIR="/data/perdue/minerva/targets"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011"
+OUTDIR="/data/perdue/minerva/tensorflow/data"
+LOGDIR="/data/perdue/minerva/tensorflow/logs"
+LOGFILE=$LOGDIR/log_hdf5_to_tfrec_minerva_xtxutuvtv${DAT}.txt
+
+# show what we will do...
+cat << EOF
+singularity exec $SNGLRTY python hdf5_to_tfrec_minerva_xtxutuvtv.py \
+  --nevents $NEVTS \
+  --max_triplets $MAXTRIPS \
+  --file_pattern $FILEPAT \
+  --in_dir $HDF5DIR \
+  --out_dir $OUTDIR \
+  --train_fraction $TRAINFRAC \
+  --valid_fraction $VALIDFRAC \
+  --logfile $LOGFILE \
+  --compress_to_gz \
+  --test_read
+EOF
+
+singularity exec $SNGLRTY python hdf5_to_tfrec_minerva_xtxutuvtv.py \
+  --nevents $NEVTS \
+  --max_triplets $MAXTRIPS \
+  --file_pattern $FILEPAT \
+  --in_dir $HDF5DIR \
+  --out_dir $OUTDIR \
+  --train_fraction $TRAINFRAC \
+  --valid_fraction $VALIDFRAC \
+  --logfile $LOGFILE \
+  --compress_to_gz \
+  --test_read
