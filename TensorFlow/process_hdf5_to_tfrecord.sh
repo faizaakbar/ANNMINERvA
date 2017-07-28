b4a588d0c35331d54259970c3bb1c6bc3a8d016b
Gabriel Perdue <gnperdue@me.com>
2017-07-28 08:50:21 -0500

    nicer options for testing in proc hdf5 to tfrec script

--- TensorFlow/process_hdf5_to_tfrecord.sh
+++ TensorFlow/process_hdf5_to_tfrecord.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
+# file creation parameters
+NEVTS=1000
+MAXTRIPS=3
+TRAINFRAC=0.88
+VALIDFRAC=0.06
+
+# tag the log file
 DAT=`date +%s`
-LOGFILE=log_hdf5_to_tfrec_minerva_xtxutuvtv${DAT}.txt
+
+# file logistics
+HDF5DIR="${HOME}/Documents/MINERvA/AI/hdf5"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011"
+OUTDIR="${HOME}/Documents/MINERvA/AI/tfrec"
+LOGDIR="${HOME}/Documents/MINERvA/AI/logs"
+LOGFILE=$LOGDIR/log_hdf5_to_tfrec_minerva_xtxutuvtv${DAT}.txt
 
 cat << EOF
 python hdf5_to_tfrec_minerva_xtxutuvtv.py \
-  --nevents 10000 \
-  --file_pattern minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_ \
-  --train_fraction 0.87 \
-  --valid_fraction 0.07 \
+  --nevents $NEVTS \
+  --max_triplets $MAXTRIPS \
+  --file_pattern $FILEPAT \
+  --in_dir $HDF5DIR \
+  --out_dir $OUTDIR \
+  --train_fraction $TRAINFRAC \
+  --valid_fraction $VALIDFRAC \
   --logfile $LOGFILE \
   --compress_to_gz \
   --test_read
 EOF
-# --max_triplets 3 \
-# --file_list minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_0000.hdf5 \
-# --dry_run
 
 python hdf5_to_tfrec_minerva_xtxutuvtv.py \
-  --nevents 10000 \
-  --file_pattern minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_ \
-  --train_fraction 0.87 \
-  --valid_fraction 0.07 \
+  --nevents $NEVTS \
+  --max_triplets $MAXTRIPS \
+  --file_pattern $FILEPAT \
+  --in_dir $HDF5DIR \
+  --out_dir $OUTDIR \
+  --train_fraction $TRAINFRAC \
+  --valid_fraction $VALIDFRAC \
   --logfile $LOGFILE \
   --compress_to_gz \
   --test_read
+# --dry_run
