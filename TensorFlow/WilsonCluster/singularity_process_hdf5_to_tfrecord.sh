3e7ee961d43b47c1b1605c7484f6e7b32655fa78
Gabriel Perdue <perdue@fnal.gov>
2017-07-28 10:11:15 -0500

    batchify singularity-based hdf5 to tfrec converter script

--- TensorFlow/WilsonCluster/singularity_process_hdf5_to_tfrecord.sh
+++ TensorFlow/WilsonCluster/singularity_process_hdf5_to_tfrecord.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#PBS -S /bin/bash
+#PBS -N tfrec_prod
+#PBS -j oe
+#PBS -o ./batchlog_tfrec_prod.txt
+# not 3 #PBS -l nodes=gpu2:gpu,walltime=24:00:00
+# not 2 #PBS -l nodes=gpu2:gpu:ppn=2,walltime=24:00:00
+# not 1 #PBS -l nodes=gpu1:gpu:ppn=2,walltime=24:00:00
+#PBS -l nodes=1:gpu,walltime=24:00:00
+# not short #PBS -l nodes=1:gpu,walltime=6:00:00
+#PBS -A minervaG
+#PBS -q gpu
+#restore to turn off email - doesn't work #PBS -m n
 
 # file creation parameters
 NEVTS=20000
@@ -21,6 +33,25 @@ FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1B
 OUTDIR="/data/perdue/minerva/tensorflow/data"
 LOGDIR="/data/perdue/minerva/tensorflow/logs"
 LOGFILE=$LOGDIR/log_hdf5_to_tfrec_minerva_xtxutuvtv${DAT}.txt
+
+# print identifying info for this job
+echo "Job ${PBS_JOBNAME} submitted from ${PBS_O_HOST} started "`date`" jobid ${PBS_JOBID}"
+cat ${PBS_NODEFILE}
+cd ${PBS_O_WORKDIR}
+echo "PBS_O_WORKDIR is `pwd`"
+GIT_VERSION=`git describe --abbrev=12 --dirty --always`
+echo "Git repo version is $GIT_VERSION"
+DIRTY=`echo $GIT_VERSION | perl -ne 'print if /dirty/'`
+if [[ $DIRTY != "" ]]; then
+  echo "Git repo contains uncomitted changes!"
+  echo ""
+  echo "Changed files:"
+  git diff --name-only
+  echo ""
+  # exit 0
+fi
+
+cp /home/perdue/ANNMINERvA/TensorFlow/hdf5_to_tfrec_minerva_xtxutuvtv.py ${PBS_O_WORKDIR}
 
 # show what we will do...
 cat << EOF
@@ -46,3 +77,6 @@ singularity exec $SNGLRTY python hdf5_to_tfrec_minerva_xtxutuvtv.py \
   --valid_fraction $VALIDFRAC \
   --logfile $LOGFILE \
   --compress_to_gz $TESTREAD
+
+echo "Job ${PBS_JOBNAME} submitted from ${PBS_O_HOST} finished "`date`" jobid ${PBS_JOBID}"
+exit 0
