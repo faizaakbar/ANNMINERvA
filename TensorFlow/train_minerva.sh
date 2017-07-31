2366cec8e03b2f26163615512542b19df571e90f
Gabriel Perdue <gnperdue@me.com>
2017-07-31 06:16:36 -0500

    fiddle with test scripts

--- TensorFlow/train_minerva.sh
+++ TensorFlow/train_minerva.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
 
 DAT=`date +%s`
-MODEL_CODE="12345"
+MODEL_CODE="20170731"
 
-# default is training, validation, and testing, no prediction
+# targets
+NCLASS=11
+TARGETS="--n_classes $NCLASS --targets_label segments"
 
+TRAINING="--nodo_training"
+TRAINING="--do_training"
+VALIDATION="--do_validaton"
+
+TESTING="--nodo_testing"
+TESTING="--do_testing"
+
+BASEP="/Users/gnperdue/Documents/MINERvA/AI/minerva_tf"
+
+PREDPATH="${BASEP}/predictions/"
+PREDFILE="$PREDPATH/predictions_mnv_st_epsilon_${NCLASS}_${MODEL_CODE}"
+PREDICTIONS="--nodo_prediction"
+PREDICTIONS="--do_prediction --pred_store_name $PREDFILE"
+
+# data, log, and model logistics
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011"
+DATADIR="${BASEP}/tfrec"
+LOGDIR="${BASEP}/logs"
+LOGFILE=$LOGDIR/log_mnv_st_epsilon_${NCLASS}_${MODEL_CODE}_${DAT}.txt
+LOGLEVEL="--log_level INFO"
+LOGLEVEL="--log_level DEBUG"
+MODELDIR="${BASEP}/models/${NCLASS}/${MODEL_CODE}"
+
+# show what we will do...
 cat << EOF
 python mnv_run_st_epsilon.py \
   --compression gz \
-  --data_dir /Users/gnperdue/Documents/MINERvA/AI/ANNMINERvA/TensorFlow \
-  --file_root minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_ \
-  --model_dir /tmp/minerva/models/${MODEL_CODE} \
-  --log_name log_mnv_st_epsilon${DAT}.txt \
-  --do_training \
-  --do_validation \
-  --nodo_testing \
-  --nodo_prediction 
+  --data_dir $DATADIR \
+  --file_root $FILEPAT \
+  --model_dir $MODELDIR \
+  --log_name $LOGFILE $LOGLEVEL \
+  $TRAINING $VALIDATION $TESTING $PREDICTIONS
 EOF
 
 python mnv_run_st_epsilon.py \
   --compression gz \
-  --data_dir /Users/gnperdue/Documents/MINERvA/AI/ANNMINERvA/TensorFlow \
-  --file_root minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_ \
-  --model_dir /tmp/minerva/models/${MODEL_CODE} \
-  --log_name log_mnv_st_epsilon${DAT}.txt \
-  --do_training \
-  --do_validation \
-  --nodo_testing \
-  --nodo_prediction 
+  --data_dir $DATADIR \
+  --file_root $FILEPAT \
+  --model_dir $MODELDIR \
+  --log_name $LOGFILE $LOGLEVEL \
+  $TRAINING $VALIDATION $TESTING $PREDICTIONS
+
+echo "Job finished "`date`""
