2482029a01d5ae02bd37ed38fe33114f7f04fa6f
Gabriel Perdue <perdue@fnal.gov>
2017-08-03 08:47:38 -0500

    script for running singularity tf with pdb

---
+++ TensorFlow/wc_debug.sh
@@ -0,0 +1,14 @@
+MODELCODE=20170803
+DAT=`date +%s`
+
+singularity exec /data/perdue/singularity/simone/ubuntu16-cuda-ml.img python -m pdb mnv_run_st_epsilon.py \
+  --compression gz \
+  --data_dir /data/perdue/minerva/tensorflow/data  \
+  --file_root minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011  \
+  --model_dir /data/perdue/minerva/tensorflow/models/11/${MODELCODE} \
+  --log_name /data/perdue/minerva/tensorflow/logs/log_mnv_st_epsilon_11_${MODELCODE}_${DAT}.txt \
+  --log_level DEBUG  \
+  --do_training \
+  --do_validaton \
+  --do_testing \
+  --do_prediction --pred_store_name /data/perdue/minerva/tensorflow/predictions//predictions_mnv_st_epsilon_11_${MODELCODE}
