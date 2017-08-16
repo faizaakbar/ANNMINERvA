e3fb6831222333a21968b2008b2c75fd7567277a
Gabriel Perdue <perdue@fnal.gov>
2017-08-16 16:17:37 -0500

    fiddling with validation

--- TensorFlow/WilsonCluster/singularity_train_minerva.sh
+++ TensorFlow/WilsonCluster/singularity_train_minerva.sh
@@ -25,20 +25,21 @@ TARGETS="--n_classes $NCLASS --targets_label segments"
 TRAINING="--nodo_training"
 TRAINING="--do_training"
 VALIDATION="--do_validaton"
+VALIDATION="--nodo_validaton"
 
-TESTING="--nodo_testing"
 TESTING="--do_testing"
+TESTING="--nodo_testing"
 
 PREDPATH="/data/perdue/minerva/tensorflow/predictions/"
 PREDFILE="$PREDPATH/predictions_mnv_st_epsilon_${NCLASS}_${MODEL_CODE}"
-PREDICTIONS="--nodo_prediction"
 PREDICTIONS="--do_prediction --pred_store_name $PREDFILE"
+PREDICTIONS="--nodo_prediction"
 
 # data, log, and model logistics
 FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011"
 FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_"
-FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_000000"
 FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_00000"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_000000"
 DATADIR="/data/perdue/minerva/tensorflow/data"
 LOGDIR="/data/perdue/minerva/tensorflow/logs"
 LOGFILE=$LOGDIR/log_mnv_st_epsilon_${NCLASS}_${MODEL_CODE}_${DAT}.txt
