f499afa0a6d9d699e3926d59b6fdeb76145fe0ef
Gabriel Perdue <perdue@fnal.gov>
2017-08-16 16:18:00 -0500

    adjust default dropout keep prob

--- TensorFlow/mnv_utils.py
+++ TensorFlow/mnv_utils.py
@@ -43,7 +43,7 @@ def make_default_train_params_dict(mnv_type='st_epsilon'):
     train_params_dict['NUM_EPOCHS'] = 1
     train_params_dict['MOMENTUM'] = 0.9
     train_params_dict['STRATEGY'] = tf.train.AdamOptimizer
-    train_params_dict['DROPOUT_KEEP_PROB'] = 0.75
+    train_params_dict['DROPOUT_KEEP_PROB'] = 0.5
     return train_params_dict
 
 
