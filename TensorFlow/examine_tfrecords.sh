cdec619973945b2e1cd29701c3dfcc873a5d902b
Gabriel Perdue <gnperdue@me.com>
2017-08-19 10:08:41 -0500

    check new files

--- TensorFlow/examine_tfrecords.sh
+++ TensorFlow/examine_tfrecords.sh
@@ -4,9 +4,9 @@
 DAT=`date +%s`
 
 # file logistics
-FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011_000000_test"
-FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011_000000_valid"
-FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_011_000002_train"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_0000_000000_train"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_0000_000000_valid"
+FILEPAT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Amc_0000_000000_test"
 DIR="/Users/gnperdue/Documents/MINERvA/AI/minerva_tf/tfrec"
 LOGFILE=log_examine_tfrec${DAT}.txt
 
