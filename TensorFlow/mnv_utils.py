0434232c4bbefa55a669dd78e05f5213ca6f45c9
Gabriel Perdue <gnperdue@me.com>
2017-08-19 10:08:20 -0500

    try a lower learning rate

--- TensorFlow/mnv_utils.py
+++ TensorFlow/mnv_utils.py
@@ -38,7 +38,7 @@ def make_default_feature_targ_dict(mnv_type='st_epsilon'):
 
 def make_default_train_params_dict(mnv_type='st_epsilon'):
     train_params_dict = {}
-    train_params_dict['LEARNING_RATE'] = 0.001
+    train_params_dict['LEARNING_RATE'] = 0.0001
     train_params_dict['BATCH_SIZE'] = BATCH_SIZE
     train_params_dict['NUM_EPOCHS'] = 1
     train_params_dict['MOMENTUM'] = 0.9
