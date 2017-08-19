1507f6bfe0e5ddd285f9690fb70f90d8d9b8f9dd
Gabriel Perdue <gnperdue@me.com>
2017-08-19 10:07:29 -0500

    test using bools directly

--- TensorFlow/scratch.py
+++ TensorFlow/scratch.py
@@ -1,6 +1,15 @@
 from __future__ import print_function
 import tensorflow as tf
 
+
+def print_alpha():
+    return 1.0
+
+
+def print_beta():
+    return 0.0
+
+
 save_every_n_batch = 5
 cntr = tf.placeholder(
     tf.int32, shape=(), name='batch_counter'
@@ -15,6 +24,14 @@ pred = tf.equal(
     tf.mod(cntr, pfrq), tfzo, name='train_valid_pred'
 )
 
+get_alpha = tf.placeholder(tf.bool, shape=(), name='batch_logic')
+ab_testing = tf.cond(
+    get_alpha,
+    print_alpha,
+    print_beta,
+    name='do_ab_testing'
+)
+
 with tf.Session() as sess:
     for i in range(10):
         print('i={}, pred={}, (i+1)mod-save={}'.format(
@@ -22,3 +39,7 @@ with tf.Session() as sess:
             sess.run([pred], feed_dict={cntr: (i + 1)}),
             (i + 1) % save_every_n_batch
         ))
+
+with tf.Session() as sess:
+    for b in [True, False, True, True, False]:
+        print(b, ', ', sess.run([ab_testing], feed_dict={get_alpha: b}))
