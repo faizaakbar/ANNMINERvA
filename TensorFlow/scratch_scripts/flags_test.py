11c841a948011de7f3d473d938f61942c6688d4f
Gabriel Perdue <gnperdue@me.com>
2017-10-09 05:48:09 -0500

    check some things

--- TensorFlow/scratch_scripts/flags_test.py
+++ TensorFlow/scratch_scripts/flags_test.py
@@ -8,12 +8,18 @@ tf.app.flags.DEFINE_string('data_dir', '/tmp/data',
                            """Directory where data is stored.""")
 tf.app.flags.DEFINE_string('file_root', 'mnv_data_',
                            """File basename.""")
+tf.app.flags.DEFINE_boolean('do_something', True,
+                            """Do a thing.""")
+tf.app.flags.DEFINE_boolean('do_something_else', False,
+                            """Do a different thing.""")
 
 
 def main(argv=None):
     import glob
     print(FLAGS.data_dir.split(','))
     print(FLAGS.file_root.split(','))
+    print(FLAGS.do_something)
+    print(FLAGS.do_something_else)
 
     dirs = FLAGS.data_dir.split(',')
     fils = FLAGS.file_root.split(',')
