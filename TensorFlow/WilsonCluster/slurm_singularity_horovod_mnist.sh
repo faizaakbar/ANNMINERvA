4673275cf2271d2105c6720fc87c237d3fa97750
Gabriel Perdue <perdue@fnal.gov>
2017-12-09 15:31:57 -0600

    script to run horovod tests

---
+++ TensorFlow/WilsonCluster/slurm_singularity_horovod_mnist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "started "`date`" "`date +%s`""
+
+nvidia-smi -L
+
+# pick up singularity v2.2
+export PATH=/usr/local/singularity/bin:$PATH
+# which singularity image
+SNGLRTY="/data/perdue/singularity/tf_1_4.simg"
+
+# get mpirun in PATH
+export PATH=/usr/local/openmpi/bin:$PATH
+
+cp -v /home/perdue/ANNMINERvA/TensorFlow/horovod_*.py `pwd`
+
+# show what we will do...
+cat << EOF
+singularity exec $SNGLRTY python horovod_test.py
+EOF
+# do the thing...
+singularity exec $SNGLRTY python horovod_test.py
+
+# show what we will do...
+cat << EOF
+mpirun -np 1 -H localhost -bind-to core -map-by core singularity exec $SNGLRTY python horovod_mnist.py
+EOF
+# do the thing...
+mpirun -np 1 -H localhost -bind-to core -map-by core singularity exec $SNGLRTY python horovod_mnist.py
+
+nvidia-smi
+
+echo "finished "`date`" "`date +%s`""
+exit 0
