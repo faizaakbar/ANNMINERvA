310e9169a3ecab4988ad6a2d1e20814acc278851
Gabriel Perdue <perdue@fnal.gov>
2017-12-19 16:56:11 -0600

    specify nodes

--- TensorFlow/WilsonCluster/sbatch_job_horovod_test.sh
+++ TensorFlow/WilsonCluster/sbatch_job_horovod_test.sh
@@ -2,10 +2,12 @@
 
 NGPU=2
 
+NODES=gpu4
+
 # show what we will do...
 cat << EOF
-sbatch --gres=gpu:${NGPU} slurm_singularity_horovod_mnist.sh
+sbatch --gres=gpu:${NGPU} --nodelist=${NODES} slurm_singularity_horovod_mnist.sh
 EOF
 
 # do the thing, etc.
-sbatch --gres=gpu:${NGPU} slurm_singularity_horovod_mnist.sh
+sbatch --gres=gpu:${NGPU} --nodelist=${NODES} slurm_singularity_horovod_mnist.sh
