ea99a93a84173737e62179c9da65f2e83fbd658b
Gabriel Perdue <perdue@fnal.gov>
2018-01-05 11:58:53 -0600

    remove PBS training scripts since we don't have/use PBS anymore

--- TensorFlow/WilsonCluster/qsub_job_train_minerva.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DAT=`date +%s`
-OUTFILENAME="./batchlog_singularity_train_minerva${DAT}.txt"
-JOBNAME="train_minerva${DAT}"
-qsub -o $OUTFILENAME singularity_train_minerva.sh -N $JOBNAME
