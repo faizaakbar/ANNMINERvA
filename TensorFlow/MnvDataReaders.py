2724dea768cc6e837cb7ace141fb3e96c7602589
Gabriel Perdue <gnperdue@me.com>
2018-01-24 10:13:52 -0600

    more work on new hdf5->tfrec

--- TensorFlow/MnvDataReaders.py
+++ TensorFlow/MnvDataReaders.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import tensorflow as tf
 
+from MnvDataConstants import EVENT_DATA
+from MnvDataConstants import EVENTIDS, PLANECODES, SEGMENTS, ZS
+
 
 class MnvTFRecordReaderBase:
     """
@@ -44,6 +47,28 @@ class MnvTFRecordReaderBase:
         _, tfrecord = reader.read(file_queue)
         return tfrecord
 
+    def _decode_eventids(self, tfrecord_features):
+        evtids = tf.decode_raw(tfrecord_features[EVENTIDS], tf.int64)
+        return evtids
+
+    def _decode_planecodes(self, tfrecord_features):
+        pcodes = tf.decode_raw(
+            tfrecord_features[PLANECODES], tf.int32
+        )
+        pcodes = tf.one_hot(
+            indices=pcodes, depth=self.n_planecodes, on_value=1, off_value=0
+        )
+        return pcodes
+
+    def _decode_segments(self, tfrecord_features):
+        segs = tf.decode_raw(tfrecord_features[SEGMENTS], tf.uint8)
+        segs = tf.one_hot(indices=segs, depth=11, on_value=1, off_value=0)
+        return segs
+
+    def _decode_zs(self, tfrecord_features):
+        zs = tf.decode_raw(tfrecord_features[ZS], tf.float32)
+        return zs
+
 
 class MnvDataReaderVertexST(MnvTFRecordReaderBase):
     """
@@ -62,13 +87,13 @@ class MnvDataReaderVertexST(MnvTFRecordReaderBase):
             planecodes_batch, segments_batch, zs_batch
     ):
         batch_dict = {}
-        batch_dict['eventids'] = eventids_batch
+        batch_dict[EVENTIDS] = eventids_batch
         batch_dict[self.imgdat_names['x']] = hitimesx_batch
         batch_dict[self.imgdat_names['u']] = hitimesu_batch
         batch_dict[self.imgdat_names['v']] = hitimesv_batch
-        batch_dict['planecodes'] = planecodes_batch
-        batch_dict['segments'] = segments_batch
-        batch_dict['zs'] = zs_batch
+        batch_dict[PLANECODES] = planecodes_batch
+        batch_dict[SEGMENTS] = segments_batch
+        batch_dict[ZS] = zs_batch
         return batch_dict
 
     def _tfrecord_to_graph_ops_et(self, num_epochs):
@@ -80,18 +105,21 @@ class MnvDataReaderVertexST(MnvTFRecordReaderBase):
             tfrecord_features = tf.parse_single_example(
                 tfrecord,
                 features={
-                    'eventids': tf.FixedLenFeature([], tf.string),
+                    EVENTIDS: tf.FixedLenFeature([], tf.string),
                     self.imgdat_names['x']: tf.FixedLenFeature([], tf.string),
                     self.imgdat_names['u']: tf.FixedLenFeature([], tf.string),
                     self.imgdat_names['v']: tf.FixedLenFeature([], tf.string),
-                    'planecodes': tf.FixedLenFeature([], tf.string),
-                    'segments': tf.FixedLenFeature([], tf.string),
-                    'zs': tf.FixedLenFeature([], tf.string),
+                    PLANECODES: tf.FixedLenFeature([], tf.string),
+                    SEGMENTS: tf.FixedLenFeature([], tf.string),
+                    ZS: tf.FixedLenFeature([], tf.string),
                 },
                 name=self.name+'_data'
             )
-            with tf.variable_scope(self.name + '_eventids'):
-                evtids = tf.decode_raw(tfrecord_features['eventids'], tf.int64)
+            with tf.variable_scope(self.name + '_' + EVENT_DATA):
+                evtids = self._decode_eventids(tfrecord_features)
+                pcodes = self._decode_planecodes(tfrecord_features)
+                segs = self._decode_segments(tfrecord_features)
+                zs = self._decode_zs(tfrecord_features)
             with tf.variable_scope(self.name + '_hitimes'):
                 hitimesx = self._process_hitimes(
                     tfrecord_features[self.imgdat_names['x']],
@@ -105,21 +133,7 @@ class MnvDataReaderVertexST(MnvTFRecordReaderBase):
                     tfrecord_features[self.imgdat_names['v']],
                     [-1, self.img_shp[3], self.img_shp[0], self.img_shp[2]]
                 )
-            with tf.variable_scope(self.name + '_planecodes'):
-                pcodes = tf.decode_raw(
-                    tfrecord_features['planecodes'], tf.int32
-                )
-                pcodes = tf.one_hot(
-                    indices=pcodes, depth=self.n_planecodes,
-                    on_value=1, off_value=0
-                )
-            with tf.variable_scope(self.name + '_segments'):
-                segs = tf.decode_raw(tfrecord_features['segments'], tf.uint8)
-                segs = tf.one_hot(
-                    indices=segs, depth=11, on_value=1, off_value=0
-                )
-            with tf.variable_scope(self.name + '_zs'):
-                zs = tf.decode_raw(tfrecord_features['zs'], tf.float32)
+
         return evtids, hitimesx, hitimesu, hitimesv, pcodes, segs, zs
 
     def batch_generator(self, num_epochs=1):
