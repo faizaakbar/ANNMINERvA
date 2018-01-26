665697723a6b44719c48a61f3fdf33f25547c369
Gabriel Perdue <gnperdue@me.com>
2018-01-26 12:14:38 -0600

    generate group and dataset dict dynamically

--- TensorFlow/MnvHDF5.py
+++ TensorFlow/MnvHDF5.py
@@ -1,9 +1,6 @@
 import h5py
 import numpy as np
-import tensorflow as tf
 import logging
-from MnvDataConstants import *
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -13,10 +10,11 @@ class MnvHDF5Reader:
     the `minerva_hdf5_reader` will return numpy ndarrays of data for given
     ranges. user should call `open()` and `close()` to start/finish.
     """
-    def __init__(self, hdf5_file, data_dict):
+    def __init__(self, hdf5_file):
         self.file = hdf5_file
         self._f = None
-        self._dd = data_dict
+        self._dd = None
+        self._groups = None
 
     def open(self):
         LOGGER.info("Opening hdf5 file {}".format(self.file))
@@ -28,23 +26,36 @@ class MnvHDF5Reader:
                     np.dtype(self._f[group][dset]),
                     np.shape(self._f[group][dset])
                 ))
+        self._groups = [group for group in self._f]
+        self._dd = {}
+        for g in self._groups:
+            self._dd[g] = [dset for dset in self._f[g]]
 
     def close(self):
         try:
             self._f.close()
+            self._dd = None
+            self._groups = None
         except AttributeError:
             LOGGER.info('hdf5 file is not open yet.')
 
     def get_data(self, name, start_idx, stop_idx):
         try:
-            group = self._dd[name]['group']
+            for g in self._groups:
+                if name in self._dd[g]:
+                    group = g
+                    break
+            else:
+                raise ValueError(
+                    '{} is not present in any group.'.format(name)
+                )
             return self._f[group][name][start_idx: stop_idx]
         except KeyError:
             LOGGER.info('{} data is not available in this HDF5 file.')
             return []
 
-    def get_nevents(self, g=EVENT_DATA):
-        sizes = [self._f[g][d].shape[0] for d in self._f[g]]
+    def get_nevents(self, group):
+        sizes = [self._f[group][d].shape[0] for d in self._f[group]]
         if min(sizes) != max(sizes):
             msg = "All dsets must have the same size!"
             LOGGER.error(msg)
@@ -52,73 +63,25 @@ class MnvHDF5Reader:
         return sizes[0]
 
 
-def make_mnv_data_dict(list_of_groups):
-    """
-    create a dict of fields to extract from the hdf5 with target dtypes.
-    """
-    # eventids are really (in the hdf5) uint64, planecodes are really uint16;
-    # use tf.{int64,int32,uint8} because these are the dtypes that one-hot
-    # supports (_not_ int16 or uint16, at least in TF v1.2); use int64 instead
-    # of unit64 because reshape supports int64 (and not uint64).
-    data_list = []
-    for g in list_of_groups:
-        if g in VALID_SET_OF_GROUPS:
-            if g == EVENT_DATA:
-                data_list.extend([
-                    (EVENTIDS, tf.int64, g),
-                    (PLANECODES, tf.int32, g),
-                    (SEGMENTS, tf.uint8, g),
-                    (ZS, tf.float32, g),
-                ])
-            if g == IMG_DATA:
-                data_list.extend([
-                    (HITIMESU, tf.float32, g),
-                    (HITIMESV, tf.float32, g),
-                    (HITIMESX, tf.float32, g),
-                ])
-            if g == GEN_DATA:
-                data_list.extend([
-                    (QSQRD, tf.float32, g),
-                    (WINV, tf.float32, g),
-                    (XBJ, tf.float32, g),
-                    (YBJ, tf.float32, g),
-                    (CURRENT, tf.int32, g),
-                    (INT_TYPE, tf.int32, g),
-                    (TARGETZ, tf.int32, g),
-                ])
-            if g == HADRO_DATA:
-                data_list.extend([
-                    (ESUM_CHGDKAONS, tf.float32, g),
-                    (ESUM_CHGDPIONS, tf.float32, g),
-                    (ESUM_HADMULTMEAS, tf.float32, g),
-                    (ESUM_NEUTPIONS, tf.float32, g),
-                    (ESUM_NEUTRONS, tf.float32, g),
-                    (ESUM_OTHERS, tf.float32, g),
-                    (ESUM_PROTONS, tf.float32, g),
-                    (N_CHGDKAONS, tf.int32, g),
-                    (N_CHGDPIONS, tf.int32, g),
-                    (N_HADMULTMEAS, tf.int32, g),
-                    (N_NEUTPIONS, tf.int32, g),
-                    (N_NEUTRONS, tf.int32, g),
-                    (N_OTHERS, tf.int32, g),
-                    (N_PROTONS, tf.int32, g),
-                ])
-            if g == LEPTO_DATA:
-                data_list.extend([
-                    (ESUM_ELECTRONS, tf.float32, g),
-                    (ESUM_MUONS, tf.float32, g),
-                    (ESUM_TAUS, tf.float32, g),
-                    (N_ELECTRONS, tf.int32, g),
-                    (N_MUONS, tf.int32, g),
-                    (N_TAUS, tf.int32, g),
-                ])
-        else:
-            raise ValueError('Unrecognized group')
-    mnv_data = {}
-    for datum in data_list:
-        mnv_data[datum[0]] = {}
-        mnv_data[datum[0]]['dtype'] = datum[1]
-        mnv_data[datum[0]]['byte_data'] = None
-        mnv_data[datum[0]]['group'] = datum[2]
+def test():
+    path = '/Users/perdue/Documents/MINERvA/AI/hdf5/201801/'
+    filen = 'hadmultkineimgs_127x94_me1Amc_tiny.hdf5'
+    reader = MnvHDF5Reader(path + filen)
+    try:
+        reader.open()
+        print reader.get_data('eventids', 0, 10)
+        try:
+            print reader.get_data('n_taus', 0, 10)
+        except ValueError:
+            print 'n_taus is not stored here.'
+        try:
+            print reader.get_data('n_rainbow_unicorns', 0, 10)
+        except ValueError:
+            print 'of course there are no unicorns here.'
+        reader.close()
+    except IOError:
+        print 'hdf5 file not found'
 
-    return mnv_data
+
+if __name__ == '__main__':
+    test()
