1b8ee13aef7965c7a565121b0de0735fc5a9f0d9
Gabriel Perdue <gnperdue@me.com>
2018-01-26 12:15:21 -0600

    the data 'constants' should be responsible for tracking the dset name and dtype associations

--- TensorFlow/MnvDataConstants.py
+++ TensorFlow/MnvDataConstants.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 EVENT_DATA = 'event_data'
 EVENTIDS = 'eventids'
 PLANECODES = 'planecodes'
@@ -68,3 +70,75 @@ VTXFINDING_TYPE = 'vtxfndingimgs'
 VALID_SET_OF_GROUPS = set(
     HADMULTKINE_GROUPS_DICT.keys() + VTXFINDING_GROUPS_DICT.keys()
 )
+
+
+def make_mnv_data_dict(list_of_groups):
+    """
+    create a dict of fields to extract from the hdf5 with target dtypes.
+    """
+    # eventids are really (in the hdf5) uint64, planecodes are really uint16;
+    # use tf.{int64,int32,uint8} because these are the dtypes that one-hot
+    # supports (_not_ int16 or uint16, at least in TF v1.2); use int64 instead
+    # of unit64 because reshape supports int64 (and not uint64).
+    data_list = []
+    for g in list_of_groups:
+        if g in VALID_SET_OF_GROUPS:
+            if g == EVENT_DATA:
+                data_list.extend([
+                    (EVENTIDS, tf.int64, g),
+                    (PLANECODES, tf.int32, g),
+                    (SEGMENTS, tf.uint8, g),
+                    (ZS, tf.float32, g),
+                ])
+            if g == IMG_DATA:
+                data_list.extend([
+                    (HITIMESU, tf.float32, g),
+                    (HITIMESV, tf.float32, g),
+                    (HITIMESX, tf.float32, g),
+                ])
+            if g == GEN_DATA:
+                data_list.extend([
+                    (QSQRD, tf.float32, g),
+                    (WINV, tf.float32, g),
+                    (XBJ, tf.float32, g),
+                    (YBJ, tf.float32, g),
+                    (CURRENT, tf.int32, g),
+                    (INT_TYPE, tf.int32, g),
+                    (TARGETZ, tf.int32, g),
+                ])
+            if g == HADRO_DATA:
+                data_list.extend([
+                    (ESUM_CHGDKAONS, tf.float32, g),
+                    (ESUM_CHGDPIONS, tf.float32, g),
+                    (ESUM_HADMULTMEAS, tf.float32, g),
+                    (ESUM_NEUTPIONS, tf.float32, g),
+                    (ESUM_NEUTRONS, tf.float32, g),
+                    (ESUM_OTHERS, tf.float32, g),
+                    (ESUM_PROTONS, tf.float32, g),
+                    (N_CHGDKAONS, tf.int32, g),
+                    (N_CHGDPIONS, tf.int32, g),
+                    (N_HADMULTMEAS, tf.int32, g),
+                    (N_NEUTPIONS, tf.int32, g),
+                    (N_NEUTRONS, tf.int32, g),
+                    (N_OTHERS, tf.int32, g),
+                    (N_PROTONS, tf.int32, g),
+                ])
+            if g == LEPTO_DATA:
+                data_list.extend([
+                    (ESUM_ELECTRONS, tf.float32, g),
+                    (ESUM_MUONS, tf.float32, g),
+                    (ESUM_TAUS, tf.float32, g),
+                    (N_ELECTRONS, tf.int32, g),
+                    (N_MUONS, tf.int32, g),
+                    (N_TAUS, tf.int32, g),
+                ])
+        else:
+            raise ValueError('Unrecognized group')
+    mnv_data = {}
+    for datum in data_list:
+        mnv_data[datum[0]] = {}
+        mnv_data[datum[0]]['dtype'] = datum[1]
+        mnv_data[datum[0]]['byte_data'] = None
+        mnv_data[datum[0]]['group'] = datum[2]
+
+    return mnv_data
