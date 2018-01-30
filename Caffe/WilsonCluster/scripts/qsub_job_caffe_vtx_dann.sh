f8ee31485bd90929631ab09423a9e68a035872d4
Gabriel Perdue <gnperdue@me.com>
2018-01-30 12:19:52 -0600

    remove old Caffe scripts

--- Caffe/WilsonCluster/scripts/qsub_job_caffe_vtx_dann.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DAT=`date +%s`
-OUTFILENAME="./caffe_vtx_dann_job${DAT}.txt"
-JOBNAME="mnv-conv-${DAT}"
-qsub -o $OUTFILENAME job_caffe_vtx_dann.sh -N $JOBNAME
--- Caffe/WilsonCluster/scripts/qsub_singularity_caffe_mnist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DAT=`date +%s`
-OUTFILENAME="./caffe_mellow_whirled_job${DAT}.txt"
-JOBNAME="caffe-mellow-whirled-${DAT}"
-qsub -o $OUTFILENAME singularity_caffe_mnist.sh -N $JOBNAME
