db83a16e922863b83ea486cae29c711164a0a2e2
Gabriel Perdue <gnperdue@me.com>
2018-02-16 16:17:04 -0600

    bug fix

--- dset_visualize.py
+++ dset_visualize.py
@@ -143,7 +143,7 @@ class MnvDataReader:
             return v if len(v) else None
         for d in [PLANECODES, SEGMENTS, ZS, N_HADMULTMEAS]:
             v = get_hdf_dat(d)
-            if v:
+            if len(v):
                 data_dict[d] = v
 
         m.close()
