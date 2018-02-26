b3539015b8fca3a3adb0e635db54034aebaea317
Gabriel Perdue <gnperdue@me.com>
2018-02-26 13:02:20 -0600

    simplify the menndl net a bit (remove layer that was doing nothing)

--- mnvtf/models_menagerie.py
+++ mnvtf/models_menagerie.py
@@ -7,20 +7,20 @@ def make_menndl_633167(img_depth=2):
     """
     convpooldict = {}
     convpooldict['use_batch_norm'] = False
-    conv_padding_scheme = 'SAME'
-    pool_padding_scheme = 'SAME'
+    conv_padding_scheme = 'VALID'
+    pool_padding_scheme = 'VALID'
 
     # build 3 convolutional towers. turn off pooling at any given
     # layer by setting `convpooldict_view['poolLayerNumber'] = None`
 
     # build the '_x' tower
     convpooldict_x = {}
-    convpooldict_x['conv1'] = {}  # conv1_0
-    convpooldict_x['pool1'] = {}  # pool2_0
-    convpooldict_x['conv2'] = {}  # conv3_0
+    convpooldict_x['conv1'] = {}    # conv1_0
+    convpooldict_x['pool1'] = None  # pool2_0 (identity)
+    convpooldict_x['conv2'] = {}    # conv3_0
     convpooldict_x['pool2'] = None
-    convpooldict_x['conv3'] = {}  # conv4_0
-    convpooldict_x['pool3'] = {}  # pool5_0
+    convpooldict_x['conv3'] = {}    # conv4_0
+    convpooldict_x['pool3'] = {}    # pool5_0
     # conv1_0
     h, w, fin, fout = 8, 3, img_depth, 32
     c1_stride_h, c1_stride_w = 2, 1
@@ -29,10 +29,7 @@ def make_menndl_633167(img_depth=2):
     convpooldict_x['conv1']['strides'] = [1, c1_stride_h, c1_stride_w, 1]
     convpooldict_x['conv1']['apply_dropout'] = True
     convpooldict_x['conv1']['padding'] = conv_padding_scheme
-    # pool2_0
-    convpooldict_x['pool1']['ksize'] = [1, 1, 1, 1]
-    convpooldict_x['pool1']['strides'] = [1, 1, 1, 1]
-    convpooldict_x['pool1']['padding'] = pool_padding_scheme
+    # pool2_0 was an identity mapping
     # conv3_0
     h, w, fin, fout = 6, 7, 32, 20
     convpooldict_x['conv2']['kernels'] = [h, w, fin, fout]
@@ -57,12 +54,12 @@ def make_menndl_633167(img_depth=2):
 
     # build the '_u' tower
     convpooldict_u = {}
-    convpooldict_u['conv1'] = {}  # conv1_1
-    convpooldict_u['pool1'] = {}  # pool2_1
-    convpooldict_u['conv2'] = {}  # conv3_1
+    convpooldict_u['conv1'] = {}    # conv1_1
+    convpooldict_u['pool1'] = None  # pool2_1 (identity)
+    convpooldict_u['conv2'] = {}    # conv3_1
     convpooldict_u['pool2'] = None
-    convpooldict_u['conv3'] = {}  # conv4_1
-    convpooldict_u['pool3'] = {}  # pool5_1
+    convpooldict_u['conv3'] = {}    # conv4_1
+    convpooldict_u['pool3'] = {}    # pool5_1
     # conv1_1
     h, w, fin, fout = 8, 3, img_depth, 32
     c1_stride_h, c1_stride_w = 2, 1
@@ -71,10 +68,7 @@ def make_menndl_633167(img_depth=2):
     convpooldict_u['conv1']['strides'] = [1, c1_stride_h, c1_stride_w, 1]
     convpooldict_u['conv1']['apply_dropout'] = True
     convpooldict_u['conv1']['padding'] = conv_padding_scheme
-    # pool2_1
-    convpooldict_u['pool1']['ksize'] = [1, 1, 1, 1]
-    convpooldict_u['pool1']['strides'] = [1, 1, 1, 1]
-    convpooldict_u['pool1']['padding'] = pool_padding_scheme
+    # pool2_1 was an identity mapping
     # conv3_1
     h, w, fin, fout = 6, 7, 32, 20
     convpooldict_u['conv2']['kernels'] = [h, w, fin, fout]
@@ -99,12 +93,12 @@ def make_menndl_633167(img_depth=2):
 
     # build the '_v' tower
     convpooldict_v = {}
-    convpooldict_v['conv1'] = {}  # conv1_2
-    convpooldict_v['pool1'] = {}  # pool2_2
-    convpooldict_v['conv2'] = {}  # conv3_2
+    convpooldict_v['conv1'] = {}    # conv1_2
+    convpooldict_v['pool1'] = None  # pool2_2 (identity)
+    convpooldict_v['conv2'] = {}    # conv3_2
     convpooldict_v['pool2'] = None
-    convpooldict_v['conv3'] = {}  # conv4_2
-    convpooldict_v['pool3'] = {}  # pool5_2
+    convpooldict_v['conv3'] = {}    # conv4_2
+    convpooldict_v['pool3'] = {}    # pool5_2
     # conv1_2
     h, w, fin, fout = 8, 3, img_depth, 32
     c1_stride_h, c1_stride_w = 2, 1
@@ -113,10 +107,7 @@ def make_menndl_633167(img_depth=2):
     convpooldict_v['conv1']['strides'] = [1, c1_stride_h, c1_stride_w, 1]
     convpooldict_v['conv1']['apply_dropout'] = True
     convpooldict_v['conv1']['padding'] = conv_padding_scheme
-    # pool2_2
-    convpooldict_v['pool1']['ksize'] = [1, 1, 1, 1]
-    convpooldict_v['pool1']['strides'] = [1, 1, 1, 1]
-    convpooldict_v['pool1']['padding'] = pool_padding_scheme
+    # pool2_2 was an identity mapping
     # conv3_2
     h, w, fin, fout = 6, 7, 32, 20
     convpooldict_v['conv2']['kernels'] = [h, w, fin, fout]
@@ -148,8 +139,6 @@ def make_menndl_633167(img_depth=2):
     # finalip == logits layer
 
     # TODO - check regularization
-    convpooldict['regularizer'] = tf.contrib.layers.l2_regularizer(
-        scale=0.0001
-    )
+    convpooldict['regularizer'] = None
 
     return convpooldict
