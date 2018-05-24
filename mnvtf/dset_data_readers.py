4f43cd17217d719e16cbbebb2bd14377a7342149
Gabriel Perdue <gnperdue@me.com>
2018-05-24 12:39:26 -0500

    start a new data readers module built around the tf.data API

---
+++ mnvtf/dset_data_readers.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import tensorflow as tf
+
+from data_constants import HITIMESU, HITIMESV, HITIMESX
+from data_constants import EVENTIDS, PLANECODES
+
+
+class DsetMnvTFRecReaderBase:
+    """
+    Minerva data reader for TFRecord files using the dataset API.
+
+    Argument comments:
+    * name is typically 'train/test/valid/etc.'
+    * n_planecodes is needed to one-hot encode the planecodes
+    * compression should be 'GZIP' (or 'ZLIB?')
+    * img shape is (img_h, img_w_x, img_w_uv, img_depth)
+    """
+    def __init__(self, args_dict):
+        self.filenames_list = args_dict['FILENAMES_LIST']
+        self.batch_size = args_dict['BATCH_SIZE']
+        self.name = args_dict['NAME']
+        self.img_shp = args_dict['IMG_SHP']
+        self.n_planecodes = args_dict['N_PLANECODES']
+        self.data_format = args_dict['DATA_FORMAT']
+        # TODO - passed in compression is the wrong type (int)
+        self.compression = args_dict['FILE_COMPRESSION']
+        self.compression = 'GZIP'
+        self._features_dict = None
+
+    def _process_hitimes(self, inp, shape):
+        """ Start with a (N, C, H, W) structure, -> (N, H, W, C) """
+        tnsr = tf.reshape(tf.decode_raw(inp, tf.float32), shape)
+        if self.data_format == 'NCHW':
+            return tnsr
+        elif self.data_format == 'NHWC':
+            return tf.transpose(tnsr, [0, 2, 3, 1])
+        else:
+            raise ValueError('Invalid data format in data reader!')
+
+    def _decode_hitimesx(self, tfrecord_features):
+        return self._process_hitimes(
+            tfrecord_features[HITIMESX],
+            [-1, self.img_shp[3], self.img_shp[0], self.img_shp[1]]
+        )
+
+    def _decode_hitimesu(self, tfrecord_features):
+        return self._process_hitimes(
+            tfrecord_features[HITIMESU],
+            [-1, self.img_shp[3], self.img_shp[0], self.img_shp[2]]
+        )
+
+    def _decode_hitimesv(self, tfrecord_features):
+        return self._process_hitimes(
+            tfrecord_features[HITIMESV],
+            [-1, self.img_shp[3], self.img_shp[0], self.img_shp[2]]
+        )
+
+    def _decode_basic(self, tfrecord_features, field, tf_dtype):
+        return tf.decode_raw(tfrecord_features[field], tf_dtype)
+
+    def _decode_onehot(
+            self, tfrecord_features, field, depth, tf_dtype=tf.int32
+    ):
+        v = tf.decode_raw(tfrecord_features[field], tf_dtype)
+        v = tf.one_hot(indices=v, depth=depth, on_value=1, off_value=0)
+        return v
+
+    def _decode_onehot_capped(
+            self, tfrecord_features, field, depth, tf_dtype=tf.int32
+    ):
+        """depth becomes the cap value - should be ~ range(depth)"""
+        v = tf.decode_raw(tfrecord_features[field], tf_dtype)
+        m = (depth - 1) * tf.ones_like(v)
+        z = tf.where(v < depth, x=v, y=m)
+        z = tf.one_hot(indices=z, depth=depth, on_value=1, off_value=0)
+        return z
+
+
+class DsetMnvTFRecReaderPlanecodes:
+    def __init__(self, args_dict):
+        DsetMnvTFRecReaderBase.__init__(self, args_dict)
+        self.data_fields = [
+            HITIMESU, HITIMESV, HITIMESX, EVENTIDS, PLANECODES
+        ]
+        self._features_dict = {
+            f: tf.FixedLenFeature([], tf.string) for f in self.data_fields
+        }
+
+    def make_dataset(self, num_epochs=1, shuffle=False):
+        def parse_fn(tfrecord):
+            tfr_features = tf.parse_single_example(
+                tfrecord,
+                features=self._features_dict,
+                name=self.name+'_data'
+            )
+            hitimesx = self._decode_hitimesx(tfr_features)
+            hitimesu = self._decode_hitimesu(tfr_features)
+            hitimesv = self._decode_hitimesv(tfr_features)
+            eventids = self._decode_basic(tfr_features, EVENTIDS, tf.int64)
+            planecodes = self._decode_onehot(
+                tfr_features, PLANECODES, self.n_planecodes
+            )
+            return hitimesx, hitimesu, hitimesv, eventids, planecodes
+        dataset = tf.data.TFRecordDataset(
+            self.filenames_list, compression_type=self.compression
+        )
+        dataset = dataset.repeat(num_epochs)
+        dataset = dataset.map(parse_fn).prefetch(self.batch_size*10)
+        dataset = dataset.batch(self.batch_size)
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=self.batch_size*10)
+        return dataset
+
+    def batch_generator(self, num_epochs=1, shuffle=False):
+        ds = self.make_dataset(num_epochs, shuffle)
+        iterator = ds.make_one_shot_iterator()
+        hitimesx, hitimesu, hitimesv, eventids, planecodes = \
+            iterator.get_next()
+        return hitimesx, hitimesu, hitimesv, eventids, planecodes
+
+    def shuffle_batch_generator(self, num_epochs=1):
+        return self.batch_generator(num_epochs, shuffle=True)
